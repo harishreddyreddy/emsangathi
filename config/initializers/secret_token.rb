a108070b2c3822d1ed3129c45366f01ef1e3f8e3
Harish <harishreddyreddy@gmail.com>
2013-03-10 16:21:13 +1100

    Use SECRET_TOKEN environment variable in production

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -4,4 +4,9 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Emsangathi::Application.config.secret_token = '450696f9c4d6fb9a321fe624eaed13b4a19c30ae416b7f2a98b0f26ed86a16cd60d4c1707df39410a43e895774ba891c25899e32f1f02550e52d8b803d319271'
+if Rails.env.production? && ENV['SECRET_TOKEN'].blank?
+ raise 'SECRET_TOKEN environment variable must be set!'
+end
+
+Emsangathi::Application.config.secret_token =
+    ENV['SECRET_TOKEN'] || '450696f9c4d6fb9a321fe624eaed13b4a19c30ae416b7f2a98b0f26ed86a16cd60d4c1707df39410a43e895774ba891c25899e32f1f02550e52d8b803d319271'
